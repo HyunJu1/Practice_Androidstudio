6517ef0637ef6bcc09b396aee28a0a250ff899b1
Hyunju1 <hh960305@naver.com>
2018-11-19 17:00:13 +0900

    add project7_1

--- Project7_1/app/src/main/java/com/example/hyunju/project7_1/Main2Activity.java
+++ Project7_1/app/src/main/java/com/example/hyunju/project7_1/Main2Activity.java
@@ -1,39 +1,38 @@
 package com.example.hyunju.project7_1;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-
-import java.util.function.LongUnaryOperator;
 
 public class Main2Activity extends AppCompatActivity {
 
     LinearLayout baseLayout;
-    Button button1;
+    EditText edit1;
+    ImageView image1;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        setTitle("배경색 바꾸기");
+        setTitle("이미지 바꾸기");
         baseLayout = (LinearLayout)findViewById(R.id.baseLayout1);
-        button1 = (Button)findViewById(R.id.button1);
-
+        edit1 = (EditText) findViewById(R.id.edit1);
+        image1=(ImageView)findViewById(R.id.image1);
 
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         MenuInflater mInflater = getMenuInflater();
-        mInflater.inflate(R.menu.menu1, menu);
+        mInflater.inflate(R.menu.menu2, menu);
 
 
         return true;
@@ -42,20 +41,17 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
-            case R.id.itemRed:
-                baseLayout.setBackgroundColor(Color.RED);
+            case R.id.item1:
+                image1.setImageResource(R.drawable.cat);
                 return true;
-            case R.id.itemBlue:
-                baseLayout.setBackgroundColor(Color.BLUE);
+            case R.id.item2:
+                image1.setImageResource(R.drawable.dog);
                 return true;
-            case R.id.itemGreen:
-                baseLayout.setBackgroundColor(Color.GREEN);
+            case R.id.item3:
+                image1.setImageResource(R.drawable.rabbit);
                 return true;
-            case R.id.subRotate:
-                button1.setRotation(45);
-                return true;
-            case R.id.subSize:
-                button1.setScaleX(2);
+            case R.id.item0:
+                image1.setRotation(Float.parseFloat(edit1.getText().toString()));
                 return true;
 
 
